df4864789c31c427bde51c2cda1caceb3652b535
Mohsin Murtaza <mohsinmurtaza1402@gmail.com>
2024-08-23 13:09:36 -0500

    upload files to cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,9 +1,43 @@
 import express, { NextFunction, Request, Response } from "express";
+import path from "path";
+import cloudinary from "../config/cloudinary";
 
-const createBook = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {};
+const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  const files = req.files as { [fieldName: string]: Express.Multer.File[] };
+  const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+  const fileName = files.coverImage[0].filename;
+  const filePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    fileName
+  );
+  const uploadResult = await cloudinary.uploader.upload(filePath, {
+    filename_override: __filename,
+    folder: "book-covers",
+    format: coverImageMimeType,
+  });
+
+  const bookFileName = files.file[0].filename;
+  const bookFilePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    bookFileName
+  );
+  try {
+    const bookFileUploadResult = await cloudinary.uploader.upload(
+      bookFilePath,
+      {
+        resource_type: "raw",
+        filename_override: bookFileName,
+        folder: "book-pdf's",
+        format: "pdf",
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.json({});
+};
 
 export { createBook };
