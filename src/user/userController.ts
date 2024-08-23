e01414ae7aed04131a37e938c4cf8a9694bdc8e1
Mohsin Murtaza <mohsinmurtaza1402@gmail.com>
2024-08-23 07:46:43 -0500

    signature jwt token

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,8 @@ import express, { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -15,8 +17,17 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(error);
   }
   const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = await userModel.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+  //token generation
+  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+  });
 
-  res.json({ message: "user created" });
+  res.json({ accessToken: token });
 };
 
 export { createUser };
