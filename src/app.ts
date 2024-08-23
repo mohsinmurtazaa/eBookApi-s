6b927e84133b0a984d61fdbd10eece147522c0f0
Mohsin Murtaza <mohsinmurtaza1402@gmail.com>
2024-08-23 09:54:54 -0500

    setup multer

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,13 @@
 import express from "express";
+import bookRouter from "./book/bookRouter";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 
 const app = express();
 app.use(express.json());
 
-//Routes
-app.get("/", (req, res, next) => {
-  res.json({ message: "welcome " });
-});
 app.use("/api/users", userRouter);
+app.use("/api/books", bookRouter);
 app.use(globalErrorHandler);
 
 export default app;
